c4e85277058cafa1e392ef89549621bd67b0054e
Jiantao Hu <jh2468@cornell.edu>
2018-06-24 18:59:44 +0900

    lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,14 @@
+public class HelloNumber{
+/**	int x = 0;
+	int sum = 0;*/
+	public static void main(String[] args){
+		int x = 0;
+		int sum = 0;
+		while(x<10){
+			System.out.print(sum+" ");
+			x = x + 1;
+		}
+	}
+}
+
+		
